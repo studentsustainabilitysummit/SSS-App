6a3b07fddb1aa302a28bb1e29ef9d96bebdf5955
Ben Marshall <bmarsha1@umbc.edu>
2023-01-18 16:24:16 -0500

    Add Event.js

---
+++ app/Event.js
@@ -0,0 +1,48 @@
+export class Event {
+
+    constructor(id, name, startTime, endTime){
+        if(this.constructor === Event) {
+            throw new Error("Event class cannot be instantiated! Use InPersonEvent or OnlineEvent instead!");
+        }
+        this.id = id;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    };
+
+    overlaps(otherEvent) {
+        if(this.startTime < otherEvent.startTime) {
+            //This event starts before the other event does
+            //The events will overlap the other event starts before this event ends
+            return this.endTime > otherEvent.startTime;
+        }
+        else if(this.startTime > otherEvent.startTime) {
+            //This event starts after the other event does
+            //The events will overlap if this event starts before the other event ends
+            return this.endTime < otherEvent.startTime;
+        }
+        else {
+            //The two events start at the same time
+            return true;
+        }
+    };
+
+};
+
+export class InPersonEvent extends Event {
+
+    constructor(id, name, startTime, endTime, location){
+        super(id, name, startTime, endTime);
+        this.location = location;
+    };
+
+};
+
+export class OnlineEvent extends Event {
+
+    constructor(id, name, startTime, endTime, zoom){
+        super(id, name, startTime, endTime);
+        this.zoom = zoom;
+    }
+
+};
