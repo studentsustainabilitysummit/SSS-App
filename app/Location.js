6bc47a1b9637ad5b2d8dd9de33c4265cc48e7122
Ben Marshall <bmarsha1@umbc.edu>
2023-01-18 16:52:00 -0500

    Add Location.js

---
+++ app/Location.js
@@ -0,0 +1,9 @@
+export default class Location{
+    constructor(id, building, room, lat, lon) {
+        this.id = id;
+        this.building = building;
+        this.room = room;
+        this.lat = lat;
+        this.lon = lon;
+    };
+};
