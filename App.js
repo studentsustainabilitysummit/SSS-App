fd6d5b49fe15941bcfc9f9e7f2cddd66150c04ad
Ben Marshall <bmarsha1@umbc.edu>
2023-01-25 22:05:51 -0500

    Add LoginScreen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import MainScreen from './app/screens/MainScreen';
 import FireClient from './app/FireClient';
 import { Component } from 'react';
 import * as Font from 'expo-font';
+import LoginScreen from './app/screens/LoginScreen';
 
 let customFonts = {
   LeagueSpartan: require('./app/assets/fonts/LeagueSpartan.ttf'),
@@ -11,18 +12,23 @@ export default class App extends Component{
 
   state = {
     fontsLoaded: false,
-    databaseLoaded: false
+    databaseLoaded: false,
+    authenticated: false
   };
 
   async _loadFontsAsync() {
     await Font.loadAsync(customFonts);
     this.setState({ fontsLoaded: true });
-  }
+  };
 
   async _databaseLoadAsync() {
     await FireClient.getInstance().init();
     this.setState({ databaseLoaded: true });
   };
+
+  async _login(email, password) {
+    
+  }
 
   componentDidMount(){
     this._loadFontsAsync();
@@ -34,6 +40,14 @@ export default class App extends Component{
     if(!this.state.fontsLoaded || !this.state.databaseLoaded) {
       return null;
     }
+
+    if(!this.state.authenticated)
+    {
+      return (
+        <LoginScreen loginFn={this._login}/>
+      )
+    }
+
     return (
       <MainScreen/>
     );
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,100 @@
+import { StyleSheet, View, TextInput, Image, TouchableOpacity, Text} from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function LoginScreen({loginFn = (email, password) => {}}) {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <SafeAreaView style={styles.container} edges={['top']}>
+            <Image style={styles.image} source={require("../assets/SSSlogo1.png")}/>
+            
+            <View style={styles.inputView}> 
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Email'
+                    placeholderTextColor="white"
+                    textAlign='center'
+                    onChangeText={newEmail => {setEmail(newEmail)}}
+                />
+            </View>
+
+            <View style={styles.inputView}> 
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Password'
+                    placeholderTextColor="white"
+                    textAlign='center'
+                    secureTextEntry={true}
+                    onChangeText={newPass => {setPassword(newPass)}}
+                />
+            </View>
+
+            <TouchableOpacity 
+            style={styles.loginButton}
+            onPress={() => {loginFn(email, password)}}
+            >
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.registerButton}>
+                <Text style={styles.buttonText}>Register</Text>
+            </TouchableOpacity>
+
+        </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      inputView: {
+        backgroundColor: '#6cc743',
+        width: "60%",
+        height: 50,
+        alignItems: "center",
+        marginBottom: 20,
+        borderRadius: 10
+      },
+      textInput: {
+        flex: 1,
+        height: "auto",
+        fontFamily: "LeagueSpartan",
+        fontSize: 18,
+        color: "white"
+      },
+      image: {
+        height: 200,
+        width: 200,
+        marginBottom: 30
+      },
+      loginButton: {
+        backgroundColor: "#04a7e7",
+        width: "80%",
+        borderRadius: 10,
+        height: 60,
+        marginTop: 50,
+        marginBottom: 20,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      registerButton: {
+        backgroundColor: "#fa6464",
+        width: "80%",
+        borderRadius: 10,
+        height: 60,
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      buttonText: {
+        color: "white",
+        fontFamily: "LeagueSpartan",
+        fontSize: 22
+      },
+})
