332441797bd1ff35878b1e4cde6f7271a809a8f8
Ben Marshall <bmarsha1@umbc.edu>
2023-01-31 20:28:48 -0500

    Set up data persistence

--- app/FireClient.ts
+++ app/FireClient.ts
@@ -1,9 +1,9 @@
-import database from '@react-native-firebase/database';
 import BuildingLocation from "./BuildingLocation";
 import EventList from "./EventList";
 import Theme from "./Theme";
 import { InPersonEvent } from './Event';
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import firestore, { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
 
 interface ThemeMap {
     [index: number]: Theme;
@@ -27,6 +27,9 @@ export default class FireClient {
     themes: ThemeMap;
     locations: LocationMap;
     allEventsCallbacks: ((EventList) => void)[];
+    buildingsCollection: FirebaseFirestoreTypes.CollectionReference;
+    themesCollection: FirebaseFirestoreTypes.CollectionReference;
+    inPersonEventsCollection: FirebaseFirestoreTypes.CollectionReference;
 
     constructor() {
         this.allEvents = new EventList();
@@ -34,85 +37,65 @@ export default class FireClient {
         this.themes = {} as ThemeMap;
         this.locations = {} as LocationMap;
         this.allEventsCallbacks = [];
+        this.buildingsCollection = firestore().collection("BuildingLocation");
+        this.themesCollection = firestore().collection("Theme");
+        this.inPersonEventsCollection = firestore().collection("InPersonEvent");
+        this.updateLocations = this.updateLocations.bind(this);
+        this.updateThemes = this.updateThemes.bind(this);
+        this.updateInPersonEvents = this.updateInPersonEvents.bind(this);
+    }
+
+
+    updateLocations(querySnapshot) {
+        let locations = {} as LocationMap;
+        querySnapshot.forEach(documentSnapshot => {
+            let id = documentSnapshot.id;
+            let {building, room, lat, lon} = documentSnapshot.data();
+            locations[id] = new BuildingLocation(id, building, room, lat, lon);
+        });
+        this.locations = locations;
+    }
+
+    updateThemes(querySnapshot) {
+        let themes = {} as ThemeMap;
+        querySnapshot.forEach(documentSnapshot => {
+            let id = documentSnapshot.id;
+            let {name, color} = documentSnapshot.data();
+            themes[id] = new Theme(name, color);
+        });
+        this.themes = themes;
+    }
+
+    timeStrToDate(date: Date, string: string) {
+        let split = string.split(":");
+        date.setHours(parseInt(split[0]));
+        date.setMinutes(parseInt(split[1]));
+        date.setSeconds(0);
+    }
+
+    updateInPersonEvents(querySnapshot) {
+        let eventList = new EventList([], true);
+        querySnapshot.forEach(documentSnapshot => {
+            let id = documentSnapshot.id;
+            let {theme, topic, speaker, startTime, endTime, location} = documentSnapshot.data();
+            
+            let startDate = new Date();
+            let endDate = new Date();
+            this.timeStrToDate(startDate, startTime);
+            this.timeStrToDate(endDate, endTime);
+            
+            eventList.addEvent(new InPersonEvent(id, this.themes[theme], topic, speaker, startDate, endDate, this.locations[location]));
+        });
+        this.allEvents = eventList;
+        this.allEventsCallbacks.forEach(f => {f(eventList)});
     }
 
     async getApplicationData(){
+        await this.buildingsCollection.get().then(this.updateLocations);
+        await this.themesCollection.get().then(this.updateThemes);
+        await this.inPersonEventsCollection.get().then(this.updateInPersonEvents);
 
-        function updateLocations(snapshot) {
-            let client = FireClient.getInstance();
-            let locations = {} as LocationMap;
-            snapshot.forEach(l => {
-                if(!l.val()) {
-                    return undefined;
-                }
-                let id = l.key;
-                let {building, room, lat, lon} = l.val();
-                locations[id] = new BuildingLocation(id, building, room, lat, lon);
-                return undefined;
-            });
-            client.locations = locations;
-        }
-
-        function updateThemes(snapshot) {
-            let client = FireClient.getInstance();
-            let themes = {} as ThemeMap;
-            snapshot.forEach(t => {
-                if(!t.val()) {
-                    return undefined;
-                }
-                let id = t.key;
-                let {name, color} = t.val();
-                themes[id] = new Theme(name, color);
-                return undefined;
-            });
-            client.themes = themes;
-        }
-
-        function timeStrToDate(date: Date, string: string) {
-            let split = string.split(":");
-            date.setHours(parseInt(split[0]));
-            date.setMinutes(parseInt(split[1]));
-            date.setSeconds(0);
-        }
-
-        function updateInPersonEvents(snapshot) {
-            let client = FireClient.getInstance();
-            let eventList = new EventList([], true);
-            snapshot.forEach(e => {
-                if(!e.val()) {
-                    return undefined;
-                }
-                let id = e.key;
-                let {theme, topic, speaker, startTime, endTime, location} = e.val();
-                
-                let startDate = new Date();
-                let endDate = new Date();
-                timeStrToDate(startDate, startTime);
-                timeStrToDate(endDate, endTime);
-                
-                eventList.addEvent(new InPersonEvent(id, client.themes[theme], topic, speaker, startDate, endDate, client.locations[location]));
-                
-                return undefined;
-            });
-            client.allEvents = eventList;
-            client.allEventsCallbacks.forEach(f => {f(eventList)});
-        }
-
-        await database()
-        .ref("/BuildingLocation")
-        .once("value", updateLocations);
-
-        await database()
-        .ref("/Theme")
-        .once("value", updateThemes);
-
-        await database()
-        .ref("/InPersonEvent")
-        .once("value", updateInPersonEvents);
-
-        database()
-        .ref("/InPersonEvent")
-        .on("value", updateInPersonEvents);
+        this.inPersonEventsCollection.onSnapshot(this.updateInPersonEvents);
     }
 
     registerAllEventsCallback(f: (EventList: any) => void) {
