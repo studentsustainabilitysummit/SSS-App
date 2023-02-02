ac3d404c234de426e775ad9a83aed5f91ac98822
Ben Marshall <bmarsha1@umbc.edu>
2023-02-02 18:31:42 -0500

    Put information on EventScreen

--- app/screens/EventScreen.js
+++ app/screens/EventScreen.js
@@ -1,15 +1,25 @@
-import { StyleSheet,} from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import React from 'react'
 import HeaderBackButtonView from '../views/HeaderBackButtonView'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { InPersonEvent } from '../Event';
 
 export default function EventScreen({route, navigation}) {
 
   let {event} = route.params;
 
+  
+
   return (
     <SafeAreaView style={styles.container} edges={['top']}>
       <HeaderBackButtonView navigation={navigation} title={"Event"}/>
+      <View style={styles.body}>
+        <Text>Topic: {event.topic}</Text>
+        <Text>Speaker: {event.speaker}</Text>
+        <Text>Theme: {event.theme.name}</Text>
+        <Text>Time: {event.getTimeRangeString()}</Text>
+        <Text>{event instanceof InPersonEvent? "Room: " + event.location.room : ""}</Text>
+      </View>
     </SafeAreaView>
   )
 }
@@ -17,8 +27,13 @@ export default function EventScreen({route, navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-  }
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  body: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
 })
