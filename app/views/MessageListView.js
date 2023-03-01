f3788b4aeaa0e8d96d63c35bbfbb5421717b7497
Ben Marshall <bmarsha1@umbc.edu>
2023-02-28 19:44:51 -0500

    Make messages scroll to bottom when new message is sent

--- app/views/MessageListView.js
+++ app/views/MessageListView.js
@@ -1,16 +1,34 @@
-import { StyleSheet, FlatList } from 'react-native'
-import React, { useEffect, useRef, useState,  } from 'react'
+import { FlatList, Keyboard } from 'react-native'
+import React, { useEffect, useRef } from 'react'
 import FireClient from '../FireClient';
 import MessageView from './MessageView';
 
 export default function MessageListView({style, messages, event}) {
 
+  const ref = useRef();
+  const fireclient = FireClient.getInstance();
+
+  const scrollToBottom = (animated=true) => {
+    if(messages.length > 0) {
+      ref.current.scrollToEnd({animated});
+    }
+  }
+
+  useEffect(() => {
+    if(ref.current.offset < 70 || (messages.length > 0 && messages[messages.length - 1].sender === fireclient.user.email)) {
+      scrollToBottom();
+    }
+
+  });
+
   return (
     <FlatList
-    style={style}
-    inverted
-    data={[...messages].reverse()} 
-    renderItem={({item}) => <MessageView message={item} event={event}/>}
+      style={style}
+      ref={ref}
+      data={messages} 
+      renderItem={({item}) => <MessageView message={item} event={event}/>}
+      onScroll={(e) => {ref.current.offset = e.nativeEvent.contentOffset.y}}
+      onContentSizeChange={() => {scrollToBottom(false)}}
     />
   )
 }
