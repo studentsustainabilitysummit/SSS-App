7a6e90409959ce653ae376aacc660fc1ee8e3fae
Ben Marshall <bmarsha1@umbc.edu>
2023-03-06 20:49:20 -0500

    Move google-services file for CI builds

--- pre-install.sh
+++ pre-install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "$(pwd)"
-
 if [[ "$EAS_BUILD_PLATFORM" == "android" ]]; then
-  echo $GOOGLE_SERVICES_BASE64 | base64 --decode > google-services.json
+  echo $GOOGLE_SERVICES_BASE64 | base64 --decode > android/app/google-services.json
 elif [[ "$EAS_BUILD_PLATFORM" == "ios" ]]; then
   echo $GOOGLE_SERVICES_INFO_BASE64 | base64 --decode > GoogleService-Info.plist
 fi
-
-echo "$(ls)"
